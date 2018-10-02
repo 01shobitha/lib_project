6775b184021fb2d21fdb63a034afe6c3e950c76e
Shobitha-M <shobithastar@gmail.com>
2018-10-02 13:49:43 +0530

    Added inline listing of 'Book' items to 'Author'

--- catalog/admin.py
+++ catalog/admin.py
@@ -6,6 +6,9 @@ from catalog.models import Author, Genre, Book, BookInstance, Language
 #admin.site.register(Book)
 class BooksInstanceInline(admin.TabularInline):
     model = BookInstance
+class BookInline(admin.TabularInline):
+    model= Book
+
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'display_genre')
@@ -14,10 +17,12 @@ class BookAdmin(admin.ModelAdmin):
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
     fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+    inlines=[BookInline]
 
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
     list_filter = ('status', 'due_back')
+    list_display = ('book', 'status', 'due_back')
 
     fieldsets = (
         (None, {
