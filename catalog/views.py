04c57cb177eb67ed2596e104854e9eb7db7635b5
01shobitha <01shobitha@gmail.com>
2018-10-15 11:27:56 +0530

    overrode title block and added count of genres

--- catalog/views.py
+++ catalog/views.py
@@ -8,6 +8,8 @@ def index(request):
     # Generate counts of some of the main objects
     num_books = Book.objects.all().count()
     num_instances = BookInstance.objects.all().count()
+    num_genres = Genre.objects.all().count()
+        
 
     # Available books (status = 'a')
     num_instances_available = BookInstance.objects.filter(status__exact='a').count()
@@ -20,6 +22,7 @@ def index(request):
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
+        'num_genres':num_genres,
     }
 
     # Render the HTML template index.html with the data in the context variable
